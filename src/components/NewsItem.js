18e88e1e8beb0a7bf3a6043b60f46a0bf660a2cd
Ankit <ankitchobdar@gmail.com>
2022-05-07 13:10:26 +0530

    Added new & previous buttons and api call

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -7,11 +7,11 @@ export class NewsItem extends Component {
     return (
       <div className="my-3">
         <div className="card" style={{width: "18rem"}}>
-            <img src={imageUrl} className="card-img-top" alt="..." />
+            <img src={!imageUrl?"https://images.livemint.com/img/2022/05/07/600x338/20211102-MAM-SSS-MN-Dhanteras-01-01-0_1643605131049_1651883906541.jpg":imageUrl} className="card-img-top" alt="..." />
             <div className="card-body">
-                <h5 className="card-title">{title}...</h5>
-                <p className="card-text">{description}...</p>
-                <a href={newsUrl} target="_blank" className="btn btn-sm btn-primary">Read More</a>
+                <h5 className="card-title">{title}</h5>
+                <p className="card-text">{description}</p>
+                <a href={newsUrl} target="_blank" className="btn btn-sm btn-dark" rel="noreferrer">Read More</a>
             </div>
         </div>
       </div>
